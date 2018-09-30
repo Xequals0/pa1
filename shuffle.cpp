215d50d9e824521af074f1a176ba542da5060d94
Xequals0 <anirudhtunoori@gmail.com>
2018-09-30 02:49:36 -0400

    Add files via upload
    update 1.0

--- shuffle.cpp
+++ shuffle.cpp
@@ -1,28 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <iostream> 
 #include <string> 
 #include <iterator>
 #include <map>
 #include <utility>
+
+
 using namespace std;
 
 //g++ -W -Wall -pedantic -o shuffle shuffle.cpp
 
-int shuffle(multimap <string, int> *ptr[])
+int wordShuffle(multimap <string, int> *ptr[], int num_maps, int num_reduces)
 {
-     
-     multimap <string, int> b1 = *ptr[3];
+         
+     int i = 0; int part_size = 0;
+     char* partition;
+     multimap <string, int> b1;
      multimap <string, int> :: iterator itr;
-     for (itr = b1.begin(); itr != b1.end(); itr++) 
-     { 
-        cout  <<  '\t' << itr->first  
-              <<  '\t' << itr->second << '\n'; 
-     } 
-     cout << endl;
+     for(i = 0; i < num_maps; i++){
+        b1 = *ptr[i]; 
+        for (itr = b1.begin(); itr != b1.end(); itr++) 
+        { 
+             cout  <<  '\t' << itr->first  
+                   <<  '\t' << itr->second << '\n'; 
+        } 
+        cout << endl;
+     }
+    
+     int split_val = 0;
+     if(num_reduces == 1) {
+         char partition [] = {'z'};
+         part_size = 1;
+     } else if(num_reduces == 2) {
+         char partition [] = {'n'};
+         part_size = 1;
+     } else if(num_reduces == 14)
+     {
+         char partition [] = {'b','d','f','g','i','k','m','o','q','s','u','w','y'};
+         part_size = 13;
+     }
+     else
+      {
+          char* partition = new char[num_reduces - 1];
+          split_val = floor(26 / (num_reduces - 1));
+          for(i = 0; i < (num_reduces - 1); i++)
+          {
+              if(i == 0)
+              {  
+                   partition[i] = 'a' + split_val;
+              }
+              else
+              {
+                   partition[i] = partition[i -1] + split_val;
+              }
+              // cout  << "hi\t" << partition[i];
+              // cout  << endl;
+          } part_size = num_reduces;
+      }
+      
+      multimap <string, int> *toReduce = new multimap <string, int> [num_reduces];
+      
+    /*  *toReduce[1].insert(make_pair <string, int> ("SQL", 2));
+      itr = toReduce[1].begin();
+           cout << "\n hi" << itr -> first;
+           cout  << endl;*/
+      
+
+      multimap <string, int> current;
+      multimap <string, int> :: iterator traverse;
+      int index = 0; i = 0;
+      for(i = 0; i < num_maps; i++){
+        current = *ptr[i]; 
+        for (traverse = current.begin(); traverse != current.end(); traverse++) 
+        { 
+             string word = traverse -> first;
+             int j = 0;
+
+             for(j = 0; j < part_size; j++)
+             {
+                 cout << partition[j] << '\n';
+             }
+
+             for(j = 0; j < part_size; j++)
+             {
+                  //cout << word.at(0) << "," << partition[j] << '\n';
+                  if(word.at(0) < partition[j])
+                  {
+                       index = j;
+                       break;
+                  }
+                  else if((j == part_size - 1) || (word.at(0) >= partition[part_size - 1]))
+                  {
+                       index = num_reduces - 1;
+
+/*
+                  cout << "hi: " << index << '\n';
+                  cout << endl;
+
+*/
+
+                       break;
+                  }
+                 // cout << index << '\n';
+                 // cout << endl;
+             }
+             *toReduce[index].insert(make_pair <string, int> (traverse -> first, traverse -> second));
+        } 
+     }
      
-
-
+     
+     multimap <string, int> :: iterator temp;
+     for(i = 0; i < num_reduces; i++){
+        for (temp = toReduce[i].begin(); temp != toReduce[i].end(); temp++) 
+        { 
+             cout  <<  '\t' << temp->first  
+                   <<  '\t' << temp->second << '\n'; 
+        } cout << "next: ";
+        cout << endl;
+     }
 
 
 
@@ -36,7 +133,7 @@ int main()
      cout << s << endl;
 
      int num_maps = 4;
-     int num_reduces = 6;
+     int num_reduces = 14;
 
 
      int te = 1;
@@ -56,8 +153,8 @@ int main()
      cout << endl;
 
      multimap <string, int> one;
-     one.insert(pair <string, int> ("SQL", 2));
-     one.insert(pair <string, int> ("DW", 1));
+     one.insert(pair <string, int> ("app", 2));
+     one.insert(pair <string, int> ("Mon", 1));
 /*
      for (itr = one.begin(); itr != one.end(); itr++) 
      { 
@@ -79,9 +176,9 @@ int main()
      cout << endl;
 */
      multimap <string, int> three;
-     three.insert(pair <string, int> ("SQL", 1));
-     three.insert(pair <string, int> ("SSAS", 1));
-     three.insert(pair <string, int> ("SSRS", 1));/*
+     three.insert(pair <string, int> ("l", 1));
+     three.insert(pair <string, int> ("g", 1));
+     three.insert(pair <string, int> ("fee", 1));/*
 
      for (itr = three.begin(); itr != three.end(); itr++) 
      { 
@@ -91,9 +188,9 @@ int main()
      cout << endl;
 */
      multimap <string, int> four;
-     four.insert(pair <string, int> ("DW", 1));
-     four.insert(pair <string, int> ("BI", 1));
-     four.insert(pair <string, int> ("SQL", 1));
+     four.insert(pair <string, int> ("kre", 1));
+     four.insert(pair <string, int> ("t", 1));
+     four.insert(pair <string, int> ("no", 1));
 /*
      for (itr = four.begin(); itr != four.end(); itr++) 
      { 
@@ -111,7 +208,7 @@ int main()
      ptr[1] = &two;
      ptr[2] = &three;
      ptr[3] = &four;
-     shuffle(ptr);
+     wordShuffle(ptr, num_maps, num_reduces);
 
      return 0;
 }
