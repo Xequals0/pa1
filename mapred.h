7702de4eb1e1bb92b1e1ef39ce514e9bfac5cf8a
Juan S. Uribe <jsu13@kill.cs.rutgers.edu>
2018-10-04 15:41:59 -0400

    added header file

---
+++ mapred.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <pthread.h>
+#include <list>
+#include <map>
+#include <iterator>
+#include <boost/algorithm/string.hpp>
+#include <vector>
+#include <math.h>
+#include <boost/serialization/map.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <sstream>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/wait.h>
+
+using namespace std;
